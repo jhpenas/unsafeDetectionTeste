08a3ac389ff17fd83d4ab4092174d8474b8a6610
Juan Rodrigo Harbar Penas <64301697+jhpenas@users.noreply.github.com>
2024-06-25 19:59:17 -0300

    Add files via upload

---
+++ groundingDINO.py
@@ -0,0 +1,76 @@
+from groundingdino.util.inference import  load_model, load_image, predict, annotate
+import groundingdino.datasets.transforms as T
+import cv2
+import torch
+import numpy as np
+from PIL import Image
+
+def preprocess_image(image_bgr: np.ndarray) -> torch.Tensor:
+    transform = T.Compose(
+        [
+            T.RandomResize([800], max_size=1333),
+            T.ToTensor(),
+            T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+        ]
+    )
+    image_pillow = Image.fromarray(cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB))
+    image_transformed, _ = transform(image_pillow, None)
+    return image_transformed
+
+
+CONFIG_PATH = 'GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py'
+WEIGHTS_PATH = 'GroundingDINO/weights/groundingdino_swint_ogc.pth'
+
+model = load_model(CONFIG_PATH,WEIGHTS_PATH)
+
+VIDEO_PATH = 'videos/forklift1.mp4'
+
+TEXT_PROMPT = 'person without hardhat, person near forklift'
+BOX_THRESHOLD = 0.35
+TEXT_THRESHOLD = 0.15
+
+OUTPUT_PATH = 'videos/output.mp4'
+
+cap = cv2.VideoCapture(VIDEO_PATH)
+
+#Get Frame and Height
+frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+#Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter(OUTPUT_PATH, fourcc, 30.0, (frame_width, frame_height))
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    #preprocess image
+    transformed_image = preprocess_image(frame)
+
+    #perform object detection on the frame
+    boxes, logits, phrases = predict(
+        model=model,
+        image=transformed_image,
+        caption=TEXT_PROMPT,
+        box_threshold=BOX_THRESHOLD,
+        text_threshold=TEXT_THRESHOLD
+    )
+
+# Annotate the frame
+    annotated_frame = annotate(image_source=frame, boxes=boxes, logits=logits, phrases=phrases)
+    annotated_frame = annotated_frame[...,::-1] # BGR to RGB
+
+    # Write the annotated frame to the output video
+    out.write(annotated_frame)
+
+    # Display the annotated frame
+    cv2.imshow("GroundingDINO Tracking", annotated_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
---
+++ yoloWorld.py
@@ -0,0 +1,36 @@
+from ultralytics import YOLO
+import cv2
+
+# Initialize a YOLO-World model
+model = YOLO("yolov8s-world.pt")  # or select yolov8m/l-world.pt for different sizes
+model.set_classes(["person near a vehicle","person without hat"])
+
+# Open the video file
+video_path = "videos/forklift1.mp4"
+cap = cv2.VideoCapture(video_path)
+
+# Loop through the video frames
+while cap.isOpened():
+    # Read a frame from the video
+    success, frame = cap.read()
+
+    if success:
+        # Run YOLOv8 tracking on the frame, persisting tracks between frames
+        results = model.track(frame, persist=True)
+
+        # Visualize the results on the frame
+        annotated_frame = results[0].plot()
+
+        # Display the annotated frame
+        cv2.imshow("YOLOv8 Tracking", annotated_frame)
+
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        # Break the loop if the end of the video is reached
+        break
+
+# Release the video capture object and close the display window
+cap.release()
+cv2.destroyAllWindows()
